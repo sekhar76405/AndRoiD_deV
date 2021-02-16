558ed258ecfaf5622c09d88a97aab1fb513ff413
sekhar76405 <ssjio5402@gmail.com>
2021-02-16 22:04:20 +0530

    added scroll view and editted buttons positioning

--- QRBarcodeScanningapp/app/src/main/java/com/example/qrbarcodescanningapp/MainActivity.java
+++ QRBarcodeScanningapp/app/src/main/java/com/example/qrbarcodescanningapp/MainActivity.java
@@ -1,22 +1,23 @@
 package com.example.qrbarcodescanningapp;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 //we have included the dependency as (implementation 'com.journeyapps:zxing-android-embedded:4.1.0')
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     private TextView textView;              //just created ref to the TextView
 
@@ -29,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.clear_btn);
         ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},
                 PackageManager.PERMISSION_GRANTED);    //asking the user to access camera(allow or deny)
-
 
         button.setOnClickListener(
                 new Button.OnClickListener() {
