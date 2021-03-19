2fff8b2a78ee3e4509028266320ae7ff740579f4
sekhar76405 <ssjio5402@gmail.com>
2021-03-19 17:09:28 +0530

    Intent from landing page to topics page addded

--- QuizApp/app/src/main/java/com/example/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/example/quizapp/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.start_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open_second_page();
+                Toast.makeText(getApplicationContext(),"Welcome",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    void open_second_page()
+    {
+        Intent intent = new Intent(this,Topics.class);
+        startActivity(intent);
+    }
+
 }
