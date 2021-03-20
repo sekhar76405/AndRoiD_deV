4c82c101f353d278362009f7dd2298ccdbe84e2d
sekhar76405 <ssjio5402@gmail.com>
2021-03-20 10:03:07 +0530

    initialised the ans buttons

--- QuizApp/app/src/main/java/com/example/quizapp/QuestionActivity.java
+++ QuizApp/app/src/main/java/com/example/quizapp/QuestionActivity.java
@@ -3,12 +3,30 @@ package com.example.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class QuestionActivity extends AppCompatActivity {
-
+    private TextView textView;
+    private Button button1,button2,button3,button4;
+    private int key = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+        textView = (TextView) findViewById(R.id.question);
+        button1 = (Button) findViewById(R.id.a);
+        button2 = (Button) findViewById(R.id.b);
+        button3 = (Button) findViewById(R.id.c);
+        button4 = (Button) findViewById(R.id.d);
+    }
+    public void receive_key(int key)
+    {
+        this.key = key;
+    }
+    
+    void topic1()
+    {
+
     }
 }
